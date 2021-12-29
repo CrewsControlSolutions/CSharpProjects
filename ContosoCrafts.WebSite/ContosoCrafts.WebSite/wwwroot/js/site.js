a7fd970f4e0997635af66786a6754ee20b557a42
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2021-12-29 15:59:34 -0700

    Added two basic projects

---
+++ ContosoCrafts.WebSite/ContosoCrafts.WebSite/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+setInterval(
+    function () {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+    }, 1000);
