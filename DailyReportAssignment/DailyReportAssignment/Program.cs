8c82bcbf582a0f44e2f0750b5a295cecc65db656
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-04 09:49:44 -0700

    Added C# solution

---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System; //utilizes the System namespace
+
+namespace DailyReportAssignment //controls the scope of the class and method, which can be very helpful for larger
+                                //projects
+{
+    class Program
+    {
+        static void Main() //no arguments exist in the parenthesis because nothing is being passed to this method
+        {
+            //daily report headers followed by asking for student name
+            Console.WriteLine("The Tech Academy\nStudent Daily Report" +
+                "\nWhat is your name?");
+            //save name as a string variable
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            //save course as a string variable
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            //save page number as a string variable
+            string pageNum = Console.ReadLine();
+            //store page number as an int
+            int pageNumConv = Convert.ToInt32(pageNum);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            //save answer as a string
+            string needsHelp = Console.ReadLine();
+            //store answer as a boolean
+            bool needsHelpConv = Convert.ToBoolean(needsHelp);
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            //save positive experience as a string
+            string posExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            //save positive experience as a string
+            string fback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            //save hours spent studying as a string
+            string hoursStudy = Console.ReadLine();
+            //store hours spent studying as an int
+            int hoursStudyConv = Convert.ToInt32(hoursStudy);
+
+            //end program with the conclusion statement
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. " +
+                "Have a great day!");
+            Console.Read();
+        }
+    }
+}
--- myConsoleProject/myConsoleProject/Program.cs
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -6,10 +6,44 @@ namespace myConsoleProject
     {
         static void Main()
         {
-            Console.WriteLine("What is your \"name?\"");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + ".");
-            Console.Read();
+            //This is a simple program
+            //Console.WriteLine("What is your \"name?\"");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Hello, " + yourName + ".");
+            //Console.Read();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string faveNum = Console.ReadLine();
+            //int faveNumConv = Convert.ToInt32(faveNum); //an example of "casting" the string to an int
+            //int total = faveNumConv + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -51;
+            char questionMark = '\u2753';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.3202404233;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureMars = -341;
+
+            string myName = "John";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
         }
     }
 }
