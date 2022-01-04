ca82409013ecae4e0f33e049b9f44199aff768c4
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-04 15:20:58 -0700

    Added solution demonstrating branching

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //display heading message and ask user for package weight
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below." +
+                "\nEnter the package weight (in lbs):");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            //display error message if weight exceeds limit
+            if (w>50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+
+            //ask user for package dimensions
+            else if (w<=50)
+            {
+                Console.WriteLine("Enter package width (in inches):");
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter package height (in inches):");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter package length (in inches):");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                //display error message is package exceeds mathematical limit
+                if (width * height * length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+
+                //display quote and end program
+                else if (width * height * length <= 50)
+                {
+                    //convert dimensions and weight calculation to decimal data type and assign to a variable
+                    //of type decimal.
+                    decimal quote = Convert.ToDecimal(length * width * height * w / 100);
+                    Console.WriteLine("The shipping cost is $" + quote+". \nThank you.");
+                }
+
+            }
+        }
+    }
+}
---
+++ IfElseStatements/IfElseStatements/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace IfElseStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 4;
+
+            //if (a == 4)
+            //{
+            //    Console.WriteLine("a equals four");
+            //}
+
+            //if (a<=4)
+            //{
+            //    Console.WriteLine("a is less than or equal to four");
+            //}
+            //if (a!=5)
+            //{
+            //    Console.WriteLine("a does not equal five");
+            //}
+            //else if (a>=7)
+            //{
+            //    Console.WriteLine("a is greater than or equal to seven");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a is greater than four but less than seven");
+            //}
+
+            //int b = 9;
+            //int c = 6;
+
+            //string result = c < b ? "c is less than b" : "c is not less than b";
+            //Console.WriteLine(result);
+
+
+
+
+            //int rTemp = 90; //actual temperature
+
+            //Console.WriteLine("hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int cTemp = Convert.ToInt32(Console.ReadLine());
+
+            //if (cTemp == rTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (cTemp > rTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature");
+            //}
+            //else if (rTemp > cTemp)
+            //{
+            //    Console.WriteLine("It is colder than room temperature");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhh.... something went wrong.");
+            //}
+
+            //string compareResults = cTemp == rTemp ? "It is room temperature." : "It is not room temperature.";
+            //Console.WriteLine(compareResults);
+
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have" +
+                "an awesome favorite number.";
+
+            Console.WriteLine(result);
+
+
+
+
+            //if (cTemp == rTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (cTemp > rTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (rTemp > cTemp)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+        }
+    }
+}
