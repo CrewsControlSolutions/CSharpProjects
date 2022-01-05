7211f85e6a6cae2a6bad08ba1a3c20a8793a058c
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-05 05:37:05 -0700

    Added solution demonstrating do-while loop

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare and initialize variables
+            int c = 0;
+            int i = 9;
+
+            //example of a while loop
+            while (c<7)
+            {
+                Console.WriteLine(c);
+                c++;
+            }
+
+            //example of a do-while loop
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 7);
+        }
+    }
+}
