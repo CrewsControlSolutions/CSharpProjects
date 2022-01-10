87930fcd63d8f8f10e6141d07a99541b90fd7f8c
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-10 14:38:22 -0700

    Added solution demonstrating use of a void method

---
+++ CallingMethods2/CallingMethods/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    public class Math
+    {
+        //this constructor is not needed for this solution but is provided for possible future use
+        public Math()
+        {
+        }
+
+        //create a method that performs a mathematical operation and displays a value to the screen
+        public void Multiply(int a, int b)
+        {
+            a = a * 2;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ CallingMethods2/CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare the variables needed for performing math operations
+            int num1;
+            int num2;
+
+            //ask user to enter first number
+            Console.WriteLine("Enter the first integer for the math operation:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //ask user to enter second number
+            Console.WriteLine("Enter the second integer for the math operation:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //instantiate an object of type "Math" and assign it to the variable called "mathOperation"
+            Math mathOperation = new Math();
+
+            //call a method in a class, passing two numbers
+            mathOperation.Multiply(num1, num2);
+
+            //call a method in a class, specifying the parameters by name.
+            //the parameter "b" is assigned the first user-provided value and the
+            //parameter "a" is assigned the second user-provided value, as a proof of concept.
+            mathOperation.Multiply(b: num1, a: num2);
+        }
+    }
+}
