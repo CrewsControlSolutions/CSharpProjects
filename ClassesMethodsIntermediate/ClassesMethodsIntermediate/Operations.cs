e8c347c58462bba322c5e1c5aec2f0717f22a072
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-10 16:09:16 -0700

    Added solution for class & methods assignment demonstrating intermediate level concepts, modified BlackJack game

---
+++ ClassesMethodsIntermediate/ClassesMethodsIntermediate/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ClassesMethodsIntermediate
+{
+    public class Operations
+    {
+        //method that receives a number and ouputs another
+        public void wickedCoolOp(int input, out int b)
+        {
+            int a = input / 2;
+            Console.WriteLine("Divide that bad boy by 2 and you get: " + a);
+
+            b = input * 2;
+        }
+
+        //example of overloading a method
+        public void wickedCoolOp(double input, out double b)
+        {
+            double a = input / 2;
+            Console.WriteLine("Divide that bad boy by 2 and you get: " + a);
+
+            b = input * 2;
+        }
+
+        //example of a static class
+        public static double boringOp(double input)
+        {
+            double a = input + 1;
+            return a;
+        }
+    }
+}
---
+++ ClassesMethodsIntermediate/ClassesMethodsIntermediate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassesMethodsIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare integers variables needed for future operations
+            int num1;
+            int num2;
+            double num3;
+            double num4;
+            double num5;
+            double num6;
+
+            //instantiate an instance of a class
+            Operations demo = new Operations();
+
+            //ask user to enter an integer
+            Console.WriteLine("Please enter an integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //call on an object's method, passing a number and outputing another number
+            demo.wickedCoolOp(num1, out num2);
+
+            //show the results of the optional output parameter
+            Console.WriteLine("Multiply that bad boy by 2 and you get: " + num2);
+
+            //ask user to enter another number that is not required to be an integer
+            Console.WriteLine("\nPlease enter a number (whole or decimal): ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            //call on an object's method, passing a number and outputing another number
+            demo.wickedCoolOp(num3, out num4);
+
+            //show the results of the optional output parameter
+            Console.WriteLine("Multiply that bad boy by 2 and you get: " + num4);
+
+            //ask user to enter another number that is not required to be an integer
+            Console.WriteLine("\nPlease enter a number (whole or decimal): ");
+            num5 = Convert.ToDouble(Console.ReadLine());
+
+            //call on an object's method, passing a number and outputing another number
+            num6 = Operations.boringOp(num5);
+
+            //show the results of the optional output parameter
+            Console.WriteLine("Add 1 to your provided value and you get: " + num6);
+        }
+    }
+}
