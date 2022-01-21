fc1b11105116e244185a4ea7537c73d2b3523329
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-21 09:48:29 -0700

    Modified Newsletter solution to include database connection

--- MVCNewsletterApp/MVCNewsletterApp/Controllers/HomeController.cs
+++ MVCNewsletterApp/MVCNewsletterApp/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using MVCNewsletterApp.Models;
+using MVCNewsletterApp.ViewModels;
 
 namespace MVCNewsletterApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = @"Data Source = (localdb)\MSSqlLocalDb; Initial Catalog = Newsletter; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -51,13 +54,66 @@ namespace MVCNewsletterApp.Controllers
                 //    connection.Close();
                 //}
 
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
                 return View("Success");
             }
         }
 
-        public IActionResult Privacy()
+        public IActionResult Admin()
         {
-            return View();
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, 
+                                    SocialSecurityNumber from SignUps";
+            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsletterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+
+                    signups.Add(signup);
+                }
+            }
+
+            var signupVms = new List<SignupVm>();
+            foreach (var signup in signups)
+            {
+                /*Map the signups*/
+                var signupVm = new SignupVm();
+                signupVm.FirstName = signup.FirstName;
+                signupVm.LastName = signup.LastName;
+                signupVm.EmailAddress = signup.EmailAddress;
+                signupVms.Add(signupVm);
+            }
+
+            return View(signupVms);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
